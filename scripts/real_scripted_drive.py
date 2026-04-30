a9d30836468c1e4611a712f41d9cd2f99efffacd
shadyP-tech <stephanus.park@stud.uni-bamberg.de>
2026-04-30 15:57:09 +0200

    change distance to 30cm

--- scripts/real_scripted_drive.py
+++ scripts/real_scripted_drive.py
@@ -203,7 +203,7 @@ def main():
             node.get_logger().warn("No tracker start pose found.")
 
         node.get_logger().info("Driving forward")
-        node.send_cmd(0.1, 0.0, 1.0)
+        node.send_cmd(0.1, 0.0, 3.0)
 
         node.get_logger().info("Done")
         node.stop()
