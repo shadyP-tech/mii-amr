ab937ce1658ba8bff2f461f72804df46d2f2e7a5
shadyP-tech <stephanus.park@stud.uni-bamberg.de>
2026-05-21 10:53:41 +0200

    patching clean exit

--- scripts/aufgabe03/lidar_obstacle_debug_viz.py
+++ scripts/aufgabe03/lidar_obstacle_debug_viz.py
@@ -633,9 +633,12 @@ def main(argv=None):
     node = LidarObstacleDebugViz(args)
     try:
         rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 
 if __name__ == "__main__":
